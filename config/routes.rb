69e33a30585741d9733c95284682895854ff3eac
harakazuki <160248478+harakazuki@users.noreply.github.com>
2024-07-14 17:05:02 +0000

    users_show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/about' => 'homes#about'
-  get 'homes/top' => 'homes#top'
+  get '/top' => 'homes#top'
   resources :posts
-  get 'users', to: 'users#index', as: 'users'
+  resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
