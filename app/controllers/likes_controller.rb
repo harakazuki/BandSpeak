e552ab1035cccb6025e14b7740a72e7025e301e4
harakazuki <160248478+harakazuki@users.noreply.github.com>
2024-10-26 14:31:34 +0000

    [WIP]posts/index_js_like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,19 +6,35 @@ class LikesController < ApplicationController
   end
 
   def create
-  like = @post.likes.new(user: current_user)
+    like = @post.likes.new(user: current_user)
 
-  if like.save
-    respond_to do |format|
-      format.html { redirect_to @post, notice: 'いいねしました。' }
-      format.js
-    end
-  else
-    respond_to do |format|
-      format.html { redirect_to @post, alert: 'いいねの追加に失敗しました。' }
-      format.js { render js: "alert('いいねの追加に失敗しました。');" }
+    if like.save
+      respond_to do |format|
+        format.html { redirect_to @post, notice: 'いいねしました。' }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @post, alert: 'いいねの追加に失敗しました。' }
+        format.js { render js: "alert('いいねの追加に失敗しました。');" }
+      end
     end
   end
+
+  def destroy
+    like = @post.likes.find_by(user: current_user)
+
+    if like&.destroy
+      respond_to do |format|
+        format.html { redirect_to @post, notice: 'いいねを解除しました。' }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @post, alert: 'いいねの解除に失敗しました。' }
+        format.js { render js: "alert('いいねの解除に失敗しました。');" }
+      end
+    end
   end
 
   private
