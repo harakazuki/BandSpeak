0b15e7f388153464c43dd2d211a4d37b522dd428
harakazuki <160248478+harakazuki@users.noreply.github.com>
2024-11-12 14:47:00 +0000

    admin_dashboard_count

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardsController < ApplicationController
   layout 'admin'
   before_action :authenticate_admin!
   def index
+    @user_count = User.count
+    @post_count = Post.count
+    @comment_count = Comment.count
   end
 end
